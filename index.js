da8a7a67b3c24ddf9b10da43dfdf7ad38b750847
JamenGilbreath <jamen.gilbreath.cohort233@gmail.com>
2023-07-15 17:34:20 -0700

    7/15

--- index.js
+++ index.js
@@ -1,59 +1,18 @@
-const canvas = document.getElementById('gameCanvas');
-const context = canvas.getContext('2d');
-const screenWidth = 480;
-const screenHeight = 320;
+import EnemyController from "./EnemyControler.js";
+const canvas = document.getElementById("Game");
+const ctx = canvas.getContext("2d");
 
-// Player variables
-const playerWidth = 20;
-const playerHeight = 10;
-let playerX = (screenWidth - playerWidth) / 2;
-const playerY = screenHeight - playerHeight - 10;
+canvas.width= 600;
+canvas.width = 600;
 
-// Keyboard event listeners
-document.addEventListener('keydown', keyDownHandler);
-document.addEventListener('keyup', keyUpHandler);
+const background = new Image();
+background.src = "images/space.png";
 
-// Keyboard input variables
-let rightPressed = false;
-let leftPressed = false;
-
-// Handle key down events
-function keyDownHandler(event) {
-  if (event.key === 'Right' || event.key === 'ArrowRight') {
-    rightPressed = true;
-  } else if (event.key === 'Left' || event.key === 'ArrowLeft') {
-    leftPressed = true;
-  }
+const enemyController = new EnemyController(canvas); 
+ 
+function game(){
+ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+enemyController.draw(ctx);
 }
 
-// Handle key up events
-function keyUpHandler(event) {
-  if (event.key === 'Right' || event.key === 'ArrowRight') {
-    rightPressed = false;
-  } else if (event.key === 'Left' || event.key === 'ArrowLeft') {
-    leftPressed = false;
-  }
-}
-
-// Game update function
-function update() {
-  // Move the player
-  if (rightPressed && playerX < screenWidth - playerWidth) {
-    playerX += 5;
-  } else if (leftPressed && playerX > 0) {
-    playerX -= 5;
-  }
-
-  // Clear the canvas
-  context.clearRect(0, 0, screenWidth, screenHeight);
-
-  // Draw the player
-  context.fillStyle = 'white';
-  context.fillRect(playerX, playerY, playerWidth, playerHeight);
-
-  // Request the next animation frame
-  requestAnimationFrame(update);
-}
-
-// Start the game
-update();
+setInterval(game, 1000/60);
